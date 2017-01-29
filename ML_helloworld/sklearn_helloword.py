e941bb005488678a77446a1b6d0f0866759cd766
korea7030 <korea7030@naver.com>
2017-01-29 14:47:45 +0900

    20170129 밑바닥부터 시작하는 딥러닝 자료

--- ML_helloworld/sklearn_helloword.py
+++
@@ -1,7 +0,0 @@
-from sklearn import tree
-features = [[140,1],[130,1], [150,0],[170,0]]
-labels = [0,0,1,1]
-clf = tree.DecisionTreeClassifier() ## 의사결정나무 분류 사용
-clf = clf.fit(features, labels) ## pattern 발견
-
-print (clf.predict([[150,0]])) # 예측
