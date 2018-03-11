c5a807939da8ca0273c5930d2262edafc97d1571
macbook <jaehyunlee@onionfive.io>
2018-03-11 12:03:21 +0900

    commit

--- askdjango/dojo/urls.py
+++ askdjango/dojo/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     url(r'^new/$', views.post_new),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit),
-    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum), # url은 무조건 문자열 타입임
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum), # url은 무조건 문자열 타입임   
     url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
     url(r'^getname/$', views.post_list1),
     url(r'^post2/$', views.post_list2),
