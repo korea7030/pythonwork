35ac77d3538ae8afbef2a05d115fbcc098c6026e
macbook <jaehyunlee@onionfive.io>
2018-07-22 14:10:56 +0900

    backjoon online 1004 commit

--- Algorithm/backjoon_1004.py
+++ Algorithm/backjoon_1004.py
@@ -1,9 +1,7 @@
-import math
-
 N = int(input())
 
 for i in range(N):
-    x1, x2, y1, y2 = map(int, input().split(' '))
+    x1, y1, x2, y2 = map(int, input().split(' '))
 
     circle_count = int(input())
     outstacle = 0
@@ -11,13 +9,17 @@ for i in range(N):
     for j in range(circle_count):
         cx, cy, r = map(int, input().split(' '))
 
-        d1 = math.sqrt((cx - x1) ** 2 + (cy - y1) ** 2)  # 행성과 출발지점과의 거리
-        d2 = math.sqrt((cx - x2) ** 2 + (cy - y2) ** 2)  # 행성과 도착지점과의 거리
+        d1 = (cx - x1) ** 2 + (cy - y1) ** 2  # 행성과 출발지점과의 거리
+        d2 = (cx - x2) ** 2 + (cy - y2) ** 2  # 행성과 도착지점과의 거리
 
-        if d1 < r:
+        if x1==x2 and y1==y2:
+            outstacle = 0
+
+        result1 = True if d1 > r*r else False
+        result2 = True if d2 > r*r else False
+
+        if result1 != result2:
             outstacle += 1
 
-        if d2 < r:
-            outstacle += 1
 
     print(outstacle)
