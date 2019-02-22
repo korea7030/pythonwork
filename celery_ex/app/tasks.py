9f6cd45e19203f9864087a7d89533cc1dc8f144a
macbook <jaehyunlee@onionfive.io>
2019-02-22 14:08:15 +0900

    delete celery_ex

--- celery_ex/app/tasks.py
+++
@@ -1,6 +0,0 @@
-from celery import shared_task
-
-# creates an instance of the task
-@shared_task
-def hello():
-    print('Hello world')
--- celery_ex/celery_ex/__init__.py
+++
@@ -1,4 +0,0 @@
-# Celery finds the tasks written when django applications start.
-from .celery import app as celery_app
-
-__all__ = ['celery_app']
