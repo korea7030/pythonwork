b06525360d2191540db70151e0fd49c64c6b7f7e
korea7030 <korea7030@naver.com>
2020-01-24 17:36:51 +0900

    add Amenity, Facility, HouseRule models from AbstractItem

--- airbnb-clone/rooms/models.py
+++ airbnb-clone/rooms/models.py
@@ -4,6 +4,38 @@ from core import models as core_models
 from users import models as user_models
 
 
+# 사용자가 변경을 하게 하기 위한 방법
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Itme Model"""
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """ RoomType Model Definition"""
+    pass
+
+
+class Amenity(AbstractItem):
+    """ Amenity Model Definition"""
+    pass
+
+
+class Facility(AbstractItem):
+    """ Facility Model Definition"""
+    pass
+
+
+class HouseRule(AbstractItem):
+    """ HouseRule Model Definition"""
+    pass
+
+
 class Room(core_models.TimeStampedModel):
     """Room definition Model"""
 
@@ -21,3 +53,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
+
+    def __str__(self):
+        return self.name
