0791d362333ff632c2596fac64283abcc0d66351
korea7030 <korea7030@naver.com>
2020-01-25 18:57:20 +0900

    view main page use django paginator

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,5 +1,6 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from . import models
 
@@ -7,22 +8,14 @@ from . import models
 def all_rooms(request):
     # page 처리(only python)
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-
-    page_size = 5
-    limit = page_size * page
-    offset = limit - page_size
-
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = models.Room.objects.count() / page_size
-
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    
     return render(
         request,
         "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": ceil(page_count),
-            "page_range": range(1, ceil(page_count)),
-        },
+        {
+            'rooms': rooms
+        }
     )
