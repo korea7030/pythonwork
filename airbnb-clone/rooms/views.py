112cc53679450ca1790ff54730890f3badd94fe4
korea7030 <korea7030@naver.com>
2020-01-25 19:17:28 +0900

    function based view finish

--- airbnb-clone/rooms/views.py
+++ airbnb-clone/rooms/views.py
@@ -1,6 +1,6 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse
 from . import models
 
@@ -10,12 +10,9 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            'rooms': rooms
-        }
-    )
+
+    try:
+        rooms = paginator.get_page(page)
+        return render(request, "rooms/home.html", {"rooms": rooms})
+    except EmptyPage:
+        return redirect("/")
