63410fe902ccdd863e7440706b15c518d6b0bc5c
korea7030 <korea7030@naver.com>
2020-02-04 05:27:59 +0900

    modify

--- Algorithm/radix_sort.py
+++ Algorithm/radix_sort.py
@@ -1,5 +1,8 @@
 # -*- config: utf-8 -*-
 
+# arr: input array
+# base: 진수표현(10진수라면 10)
+
 
 def radix_sort(arr, base):
     size = len(arr)
@@ -10,26 +13,26 @@ def radix_sort(arr, base):
     while exp <= max_val:
         count = [0] * base
 
+        # 현재 자리수를 기준으로 빈도수 세기
         for i in range(size):
             print((arr[i] // exp) % base)
             count[(arr[i] // exp) % base] += 1
 
         print('first count : ' + str(count))
 
+        # count 업데이트
         for i in range(1, base):
             count[i] += count[i-1]
 
         print('cumsum second count : ' + str(count))
 
+        # 현재 자리수를 기준으로 업데이트
         for i in range(size-1, -1, -1):
             j = (arr[i] // exp) % base
             output[count[j] - 1] = arr[i]
             count[j] -= 1
 
         print('output : ' + str(output))
-
-        for i in range(size):
-            arr[i] = output[i]
         exp *= base
 
     return output
