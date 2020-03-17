fce8d8f41bbf0fb49aa3013a72e6423b7c541d74
korea7030 <korea7030@naver.com>
2020-03-17 22:10:37 +0900

    airbnb signup development

--- airbnb-clone/users/forms.py
+++ airbnb-clone/users/forms.py
@@ -20,3 +20,30 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.ModelForm):
+    
+    class Meta:
+        model = models.User
+        fields = ['first_name', 'last_name', 'email']
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+
+    def clean_password2(self):
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+
+        if password != password2:
+            raise forms.ValidationError('password confimation does not match')
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        user.username = email
+        user.set_password(password)
+        user.save()
