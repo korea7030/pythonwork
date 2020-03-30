89ee702a3315ee1561ace06039db441ea595cc1d
korea7030 <korea7030@naver.com>
2020-03-30 22:22:41 +0900

    login, signup form add css and modify

--- airbnb-clone/users/forms.py
+++ airbnb-clone/users/forms.py
@@ -4,8 +4,10 @@ from . import models
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     # field이름에 clean_을 붙여서 함수를 만들어야 함
 
@@ -28,9 +30,28 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
         fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+        }
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
 
     def clean_password2(self):
         password = self.cleaned_data.get('password')
